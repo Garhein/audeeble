8312f1a537436b674929c5720a7477648c81611c
Xavier VILLEMIN <xavier.villemin@gmail.com>
2019-10-11 21:15:31 +0200

    MàJ des librairies externes + modifications du layout principal

--- Audeeble_Web/Controllers/HomeController.cs
+++ Audeeble_Web/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Audeeble_Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Audeeble_Web.Models;
+using System.Diagnostics;
 
 namespace Audeeble_Web.Controllers
 {
@@ -19,11 +15,6 @@ namespace Audeeble_Web.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
