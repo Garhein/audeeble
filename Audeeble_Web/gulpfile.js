28770e6908ecbd17685706ac4df3c96543814893
Xavier VILLEMIN <xavier.villemin@gmail.com>
2019-10-12 10:16:13 +0200

    Ajout de Gulp + création des tâches de base (transformation SCSS en CSS)

---
+++ Audeeble_Web/gulpfile.js
@@ -0,0 +1,46 @@
+﻿/// <binding ProjectOpened='watch' />
+
+// Chargement des dépendances
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+/***************************/
+/** Définition des tâches **/
+/***************************/
+
+// Tâche de "build"
+gulp.task('css', function () {
+    return gulp
+        .src('./wwwroot/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+// Tâche de "prod" : minification CSS
+/*
+gulp.task('minify', function () {
+    return gulp
+        .src('./wwwroot/css/*.css')
+        .pipe(plugins.csso())
+        .pipe(plugins.rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./wwwroot/css/'));
+});
+*/
+
+/**********************/
+/** Ajout des tâches **/
+/**********************/
+
+// Tâche "build"
+gulp.task('build', gulp.parallel('css'));
+
+// Tâche "prod" : Build + minify
+// gulp.task('prod', gulp.parallel('build', 'minify'));
+
+// Tâche "watch" : surveillance des fichiers SASS
+gulp.task('watch', function () {
+    gulp.watch('./wwwroot/scss/**/*.scss', gulp.parallel('build'));
+});
+
+// Tâche par défaut
+gulp.task('default', gulp.parallel('build'));
