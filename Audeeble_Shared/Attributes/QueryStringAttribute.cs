c185c695bdc667db9dedd8b36a610d17cf293790
Xavier VILLEMIN <xavier.villemin@gmail.com>
2019-10-13 11:05:21 +0200

    Génération automatique d'un QueryString
     - création attribut personnalisé
     - création classe de base des modèles des critères de recherche
     - fonction de génération par réflexion du QueryString à partir du modèle des critères de recherche

---
+++ Audeeble_Shared/Attributes/QueryStringAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Audeeble_Shared.Attributes
+{
+    /// <summary>
+    /// Attribut personnalisé permettant la génération automatique d'un <see cref="Microsoft.AspNetCore.Http.QueryString"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class QueryStringAttribute : Attribute
+    {
+        public string UriParameterName { get; set; }
+
+        /// <summary>
+        /// Constructeur.
+        /// </summary>
+        /// <param name="uriParameterName">Nom du paramètre dans l'URI.</param>
+        public QueryStringAttribute(string uriParameterName)
+        {
+            this.UriParameterName = uriParameterName;
+        }
+    }
+}
---
+++ Audeeble_Shared/Models/QueryStringModel.cs
@@ -0,0 +1,39 @@
+﻿using Audeeble_Shared.Attributes;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Reflection;
+
+namespace Audeeble_Shared.Models
+{
+    /// <summary>
+    /// Classe de base des modèles permettant de générer un <see cref="QueryString"/>.
+    /// </summary>
+    public class QueryStringModel
+    {
+        /// <summary>
+        /// Construction du composant de l'URI, en fonction de l'attribut personnalisé <see cref="QueryStringAttribute"/>.
+        /// </summary>
+        /// <returns><see cref="string"/> représentant les paramètres de l'URI.</returns>
+        public string ToUriComponent()
+        { 
+            QueryString qs            = new QueryString();            
+            Type myType               = this.GetType();
+            PropertyInfo[] properties = myType.GetProperties();
+
+            foreach (PropertyInfo pi in properties)
+            {
+                QueryStringAttribute attr = pi.GetCustomAttribute(typeof(QueryStringAttribute)) as QueryStringAttribute;
+                if (attr != null)
+                {
+                    object objValue = pi.GetValue(this);
+                    if (objValue != null)
+                    {
+                        qs = qs.Add(attr.UriParameterName, objValue.ToString());
+                    }
+                }
+            }
+            
+            return qs.ToUriComponent();
+        }
+    }
+}
