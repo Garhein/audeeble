f214adc10c8879b5947e13f712241901dd74c34d
Xavier VILLEMIN <xavier.villemin@gmail.com>
2019-10-27 21:11:09 +0100

    Retrait des fonctions JS de gestion du message d'attente

--- Audeeble_Web/wwwroot/js/global/AudeebleUI.js
+++ Audeeble_Web/wwwroot/js/global/AudeebleUI.js
@@ -3,8 +3,8 @@
 /**
  * Modification du titre de la checkbox permettant de cocher/décocher tous les éléments.
  * @author  Xavier VILLEMIN
- * @param   {object}    objCheckBox
- * @param   {boolean}   isCheck
+ * @param   {object}    objCheckBox Sélecteur de la checkbox sur laquelle appliquer le changement de titre
+ * @param   {boolean}   isCheck     Booléen indiquant si la checkbox est cochée
  */
 function AudeebleUI_ChangeTitleCheckBoxAll(objCheckBox, isCheck) {
     if ($(objCheckBox).length > 0) {
@@ -21,16 +21,15 @@ function AudeebleUI_ChangeTitleCheckBoxAll(objCheckBox, isCheck) {
 
 /**
  * Cocher/décocher toutes les checkbox présentes dans la table associée.
- * Seules les checkbox situées dans les colonnes de classe "ui-col-check" sont impactées.
+ * Seules les checkbox situées dans les colonnes de classe "ui-col-check" sont prises en compte.
  * @author  Xavier VILLEMIN
- * @param   {object} objClick
+ * @param   {object} objClick Checkbox déclenchant la fonction
  */
 function AudeebleUI_TableCheckUncheckAll(objClick) {
     // Récupération de la table
     var tbl = $(objClick).closest("table");
 
     if (tbl !== null && tbl !== undefined && tbl.length > 0) {
-
         // Récupération de l'état
         var isChecked = $(objClick).is(":checked");
 
@@ -47,14 +46,13 @@ function AudeebleUI_TableCheckUncheckAll(objClick) {
  * Cocher/déocher la checkbox principale au changement d'état d'une checkbox fille.
  * Seules les checkbox situées dans les colonnes de classe "ui-col-check" sont prises en compte.
  * @author  Xavier VILLEMIN
- * @param   {object} objClick
+ * @param   {object} objClick Checkbox déclenchant la fonction
  */
 function AudeebleUI_TableCheckUncheckValue(objClick) {
     // Récupération de la table
     var tbl = $(objClick).closest("table");
 
     if (tbl !== null && tbl !== undefined && tbl.length > 0) {
-
         // Sélecteur pour la mise à jour du titre de la case à cocher principale
         var checkAll = $("thead tr th.ui-col-check input[type=checkbox]", $(tbl));
 
@@ -77,7 +75,7 @@ function AudeebleUI_TableCheckUncheckValue(objClick) {
 
 //#endregion
 
-//#region Messages
+//#region Messages génériques
 
 /**
  * Affichage d'un message d'erreur à l'aide d'une fenêtre de dialogue générique.
@@ -144,45 +142,6 @@ function AudeebleUI_DisplayToast(content, title, autoHide = false, delay = 10) {
     }
     
     $("#ui-audeeble-toasts > div .toast:last-child").toast('show');
-}
-
-/**
- * Affichage d'un message d'attente.
- * @author  Xavier VILLEMIN
- * @param   {string} waitingMessage
- */
-function AudeebleUI_DisplayWaitingMessage(waitingMessage) {
-    // Définition des ID
-    var waitingID = "#ui-waiting";
-    var messageID = "#ui-waiting-msg";
-
-    // Définition du message
-    waitingMessage = (waitingMessage === null || waitingMessage === undefined) ? "Traitement en cours ..." : waitingMessage;
-
-    // Insertion du message
-    $(messageID + " span").html(waitingMessage);
-
-    // Ajout de la classe d'animation
-    $(messageID + " i").addClass("fa-spin");
-
-    // Affichage du message
-    $(waitingID).fadeIn("slow");
-}
-
-/**
- * Cacher le message d'attente.
- * @author  Xavier VILLEMIN
- */
-function AudeebleUI_HideWaitingMessage() {
-    // Définition des ID
-    var waitingID = "#ui-waiting";
-    var messageID = "#ui-waiting-msg";
-
-    // Retrait de la classe d'animation (économisation des ressources)
-    $(messageID + " i").removeClass("fa-spin");
-
-    // Cacher le message
-    $(waitingID).fadeOut("slow");
 }
 
 //#endregion
